d932e16f8e522a67c12036197e464a8a24aaa6cd
DaLei Sun <sundalei1988@gmail.com>
2022-12-29 10:23:31 +0800

    test

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -7,20 +7,15 @@ import com.example.view.EmployeeView;
 import com.example.view.UserIO;
 import com.example.view.UserIOConsoleImpl;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.util.Scanner;
-
 public class Main {
     
     public static void main(String[] args) {
 
         UserIO io = new UserIOConsoleImpl();
         EmployeeView employeeView = new EmployeeView(io);
-
         EmployeeDao employeeDao = new EmployeeDaoImpl();
         EmployeeController employeeController = new EmployeeController(employeeView, employeeDao);
+        
         employeeController.run();
     }
 }
